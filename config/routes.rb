28934a41aa849bf134c1676b884d0bd1fecd42c1
harjitmavi <mavi.harjit@gmail.com>
2016-11-25 22:49:48 +0000

    added index about and contact pages thru generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  root to: 'pages#index'
+
+  get 'pages/about'
+  get 'hello', to: 'pages#contact'
+
+  get 'pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
